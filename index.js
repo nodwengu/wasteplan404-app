44d099b37166109ab3108f34b4960ce72b33ec48
Tersia Koetzee <tersiakoetzee@gmail.com>
2019-12-10 14:10:29 +0200

    landing page layout

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-// parse application/x-www-form-urlencoded
+
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
